27d9e126edaaa8c15db93b48a9260b94175e920d
pshivam97 <37222952+pshivam97@users.noreply.github.com>
2018-09-02 09:16:30 +0530

    Delete a.py

--- Project 2 (EDA & Unsupervised Learning)/Code/a.py
+++
@@ -1,7 +0,0 @@
-from matplotlib import pyplot as Plot
-from sklearn import datasets
-import seaborn as sns
-sns.set(style="ticks", color_codes=True)
-iris = sns.load_dataset("iris")
-sns.pairplot(iris)
-Plot.show()
